6434d411ce0416a3315bc95cb9888d300bd6dac6
ESHWAR <eshwar@eshwar.com>
2018-03-05 23:16:13 +0530

    sparse_matrix modified

---
+++ practice.py
@@ -0,0 +1,5 @@
+list=['a','a','a','a']
+count=0
+if 'a' in list:
+	count+=1
+print(count)
--- sparse.py
+++ sparse.py
@@ -2,19 +2,23 @@ import os
 import math
 import numpy as np
 import scipy.sparse as sp
-path = '/media/eshwar/DATA/Eshwar/Projects/meddocclproject/pp2/'
+#path = '/media/eshwar/DATA/Eshwar/Projects/Text classification/Medical-document-classification/pp2/'
+#list1 = ['C01/','C02/','C03/','C04/','C05/','C06/','C07/','C08/','C09/','C10/','C11/','C12/','C13/','C14/','C15/','C16/','C17/','C18/','C19/','C20/','C21/','C22/','C23/']
 list1 = ['C01/','C02/','C03/']
 row_list=[]
 col_list=[]
 data_list=[]
+lcount=0
 tf=[]
 idf=[]
-maincount=0
-maindoccount=0
+dwtcount=0
+doc_with_t=[]
+main_col_count=0
+main_doc_count=0
 row_list_count=0
-col_list_count=0
+col_list_count=-1
 for each in list1:
-	rootdir = '/media/eshwar/DATA/Eshwar/Projects/meddocclproject/pp2/'
+	rootdir = '/media/eshwar/DATA/Eshwar/Projects/Text classification/Medical-document-classification/pp2/'
 	rootdir=rootdir+each
 	flist=[]
 	#print(rootdir)
@@ -40,14 +44,17 @@ for each in list1:
 			col_list_count+=1
 			col_list.append(col_list_count)
 			c = filelist.count(each)
+			#print(c,sep=' ')
 			l = len(filelist)
+			#print(l,sep='\t')
 			tfval=c/l
 			tf.append(round(tfval,2))
 			mainlist.append(each)
 		nod+=1
-	maindoccount+=nod
-	nodwitht=0
+	main_doc_count+=nod
+	#nodwitht=0
 	for each in mainlist:
+		nodwitht=0
 		#row_list_count+=1
 		countlist=[]
 		for ff in flist:
@@ -55,23 +62,33 @@ for each in list1:
 			line = openf.read()
 			openf.close()
 			countlist=line.split()
+			#print('countlist is printed below')
+			#print(countlist)
 			if each in countlist:
 				nodwitht+=1
-				continue
 		idf.append(round(math.log(nod/nodwitht),2))
 	#print('Length of main list: ')
 	#print(len(mainlist))
-	#maincount+=len(mainlist)
-	print('---------------------------------------------')
+	main_col_count+=len(mainlist)
+	print('---------------tf_idf--------done for one folder-------------------------'+list1[lcount])
+	lcount+=1
 print(len(tf))
 print(len(idf))
 print(len(row_list))
 print(len(col_list))
+print('main_doc_count: ')
+print(main_doc_count)
+print('main_col_count: ')
+print(main_col_count)
 #print(idf)
 #data_list=[a*b for a,b in zip(row_list,col_list)]
 from operator import mul
-data_list=list(map(mul,tf,idf))
-print(len(data_list))
+data_list_temp=[]
+data_list_temp=list(map(mul,tf,idf))
+data_list=[round(ele,2) for ele in data_list_temp]
+#print(data_list)
 #print(row_list)
-arr = sp.csr_matrix((data_list,(row_list,col_list)),shape=(5000,5000))
-print(arr)
+sparse_matrix = sp.csr_matrix((data_list,(row_list,col_list)),shape=(main_doc_count+1,main_col_count))
+#print(arr)
+sp.save_npz('/media/eshwar/DATA/Eshwar/Projects/Text classification/Medical-document-classification/sparse_matrix.npz',sparse_matrix)
+print('************sparse_matrix created and saved successfully**********')
