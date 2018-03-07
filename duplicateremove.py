c0613e1b20418aca7bf8a5be0d910566810af9f8
ESHWAR <eshwar@eshwar.com>
2018-03-07 23:54:38 +0530

    dynamic lists with folder names and files obtained

--- duplicateremove.py
+++ duplicateremove.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import re
-rootdir = '/media/eshwar/DATA/Eshwar/Projects/Text classification/Medical-document-classification/ohmfew/'
+rootdir = '/media/eshwar/DATA/Eshwar/Projects/Text classification/Medical-document-classification/ohsumed-all/'
 flist=[]
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
@@ -10,29 +10,39 @@ for subdir, dirs, files in os.walk(rootdir):
 fnamelist=[]
 for each in flist:
 	fnamelist.append(each[-7:])
-print(fnamelist)
-folder_list=os.listdir('/media/eshwar/DATA/Eshwar/Projects/Text classification/Medical-document-classification/ohmfew')
-print(folder_list)
+#print(fnamelist)
+folder_names=os.listdir(rootdir)
+print(folder_names)
 
 nodlist=[]
-for dir,subdir,files in os.walk('/media/eshwar/DATA/Eshwar/Projects/Text classification/Medical-document-classification/ohmfew/'):
+for dir,subdir,files in os.walk(rootdir):
     if str(len(files))=='0': continue
     nodlist.append(str(len(files)))
+
 nodlist=list(map(int,nodlist))
 print(nodlist)
-k=1
-c=0
-for each in folder_list:
+k=0
+c=1
+nodc=nodlist[0]
+#print(len(folder_names))
+loopcount=1
+for each in folder_names:
 	exec(each+"=[]")
-	for i in range(k,nodlist[c]):
-		exec(each.append(flist[i]))
-	k=nodlist[c]+1
+	for i in range(k,nodc):
+		exec(each+'.append(fnamelist[i])')
+	if len(folder_names)==loopcount: 
+		break
+	k=nodc
+	nodc+=nodlist[c]
 	c+=1
+	loopcount+=1
+'''
 print(C01)
+print('--------------------------')
+print(C02)
+print('-----------------------')
+print(C03)
 '''
-for i in range(k,nodlist[c]):
-	str(each).append(flist[i])
-k=nodlist[c]+1
-c+=1
-print(str(each))
-'''
+for each in folder_names:
+	print(each)
+	exec('print('+each+')')
